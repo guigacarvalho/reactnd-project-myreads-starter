0e3a1c346d65aec5b4ab0fdcf6d3fc101779045c
Guilherme Carvalho <guilherme_carvalho@homedepot.com>
2017-08-01 22:35:16 -0700

    Fixing identation

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -5,10 +5,10 @@ class BookShelf extends React.Component {
     render(){
         return (
             <div className="bookshelf">
-            <h2 className="bookshelf-title">{this.props.title}</h2>
-            <div className="bookshelf-books">
-                <BookList books={this.props.books} onChange={this.props.onChange}/>
-            </div>
+                <h2 className="bookshelf-title">{this.props.title}</h2>
+                <div className="bookshelf-books">
+                    <BookList books={this.props.books} onChange={this.props.onChange}/>
+                </div>
             </div>
         )
     }
