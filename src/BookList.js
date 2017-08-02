cfc15d2afc7f977e45cd60875e6857bbb837f5d8
Guilherme Carvalho <guilherme_carvalho@homedepot.com>
2017-08-02 00:40:42 -0700

    Making BookList a stateless component

--- src/BookList.js
+++ src/BookList.js
@@ -1,21 +1,16 @@
 import React from 'react'
 import BookItem from './BookItem.js'
 
-class BookList extends React.Component {
-    render(){
-        return (
-            <ol className="books-grid">
-                {
-                    Array.isArray(this.props.books) ? this.props.books.map((book, index) => (
-                        <li key={book.id}>
-                            <BookItem book={book} onChange={this.props.onChange}/>
-                        </li>
-                    )) : ''
-                }
-            </ol>
-
-        )
-    }
-}
+const BookList = ({books, onChange}) => (
+    <ol className="books-grid">
+        {
+            Array.isArray(books) ? books.map((book) => (
+                <li key={book.id + book.shelf}>
+                    <BookItem book={book} onChange={onChange}/>
+                </li>
+            )) : ''
+        }
+    </ol>
+)
 
 export default BookList
